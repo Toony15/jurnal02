3228728f60b0d598ca1be4f567ab814044e93312
Toony15 <mramadhanfathoni@gmail.com>
2023-09-27 12:39:25 +0700

    first commit

---
+++ tugas02.java
@@ -0,0 +1,9 @@
+public class tugas02 {
+    
+    public static void main( String [] args) {
+        System.out.println("NAMA: MUHAMMAD RAMADHAN FATHONI");
+        System.out.println("Nim: 607062300055");
+        System.out.println("Kelas: D3IF-47-03");
+
+    }
+}
